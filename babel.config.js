5724f0d1b336a514ce85caa78fb14f9852454e5c
xiezh <471595249@qq.com>
2025-02-28 14:26:11 +0800

    fix duration;

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,6 @@
 module.exports = {
     presets: ['module:metro-react-native-babel-preset'],
     plugins: [
-        [
-            'module-resolver',
-            {
-                root: './',
-                extensions: ['.tsx', '.ts', '.js', '.json'],
-                alias: {
-                    '@/': './src',
-                },
-            },
-        ],
         'react-native-reanimated/plugin',
     ],
 };
